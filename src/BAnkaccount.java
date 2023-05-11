d226eb21a2318353433c8b2614b554e1eb2ebde9
aashish kumar yadav <aashish8174@gmail.com>
2023-05-11 14:01:19 +0530

    every thing updated

--- src/BAnkaccount.java
+++ src/BAnkaccount.java
@@ -1,2 +1,7 @@
 public interface BAnkaccount {
+    // withdraw money
+    // check balance
+    //add money
+    // check roi
+    //current rate of interest
 }
--- src/SBIbankaccount.java
+++ src/SBIbankaccount.java
@@ -1,2 +1,2 @@
-public class SBIbankaccount {
+public class SBIbankaccount implements BAnkaccount{
 }
